c5d543550610504b6c52f1621cb5b8cede62d1e3
jerensl <54782057+jerensl@users.noreply.github.com>
2023-04-01 10:47:17 +0800

    set data for c implementation

--- internal/pkg/binding/c.go
+++ internal/pkg/binding/c.go
@@ -10,18 +10,22 @@ package binding
 import "C"
 import (
 	"unsafe"
-
-	"gonum.org/v1/gonum/mat"
 ) 
 
 type Data struct {
 	n 		int64
 	m 		int64
-	mat_p 	mat.Dense
-	mat_a 	mat.Dense
-	q 		float32
-	l 		float32
-	u 		float32
+	p_x 	[]C.c_float
+	p_i 	[]C.c_int
+	p_p 	[]C.c_int
+	p_nnz	C.c_int
+	a_x 	[]C.c_float
+	a_i 	[]C.c_int
+	a_p 	[]C.c_int
+	a_nnz	C.c_int
+	q 		[]C.c_float
+	l 		[]C.c_float
+	u 		[]C.c_float
 }
 
 type OSQPWorkSpace struct {
@@ -31,7 +35,6 @@ type OSQPWorkSpace struct {
 }
 
 func NewOSQP() *OSQPWorkSpace {
-
 	settings := (*C.OSQPSettings)(C.c_malloc(C.sizeof_OSQPSettings))
 
 	if settings != nil {
@@ -47,6 +50,21 @@ func (o *OSQPWorkSpace) Solve() {
 	C.osqp_solve(o.work)
 }
 
+func (o *OSQPWorkSpace) SetData(newData Data) {
+	data := (*C.OSQPData)(C.c_malloc(C.sizeof_OSQPData))
+
+	if data != nil {
+		data.n = (C.c_int)(newData.n)
+		data.m = (C.c_int)(newData.m)
+
+		data.P = C.csc_matrix(data.n, data.n, newData.p_nnz, (*C.c_float)(unsafe.Pointer(&newData.p_x[0])), (*C.c_int)(unsafe.Pointer(&newData.p_i[0])), (*C.c_int)(unsafe.Pointer(&newData.p_p[0])))
+		data.q = (*C.c_float)(unsafe.Pointer(&newData.q))
+		data.A = C.csc_matrix(data.m, data.n, newData.a_nnz, (*C.c_float)(unsafe.Pointer(&newData.a_x[0])), (*C.c_int)(unsafe.Pointer(&newData.a_i[0])), (*C.c_int)(unsafe.Pointer(&newData.a_p[0])))
+
+		data.l = (*C.c_float)(unsafe.Pointer(&newData.l[0]))
+		data.u = (*C.c_float)(unsafe.Pointer(&newData.u[0]))
+	}
+}
 
 func (o *OSQPWorkSpace) CleanUp() {
 	C.osqp_cleanup(o.work)
